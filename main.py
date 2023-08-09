2c33dcb6c56798e7a50139256bfc810514449425
opixelum <48217573+opixelum@users.noreply.github.com>
2023-08-09 16:02:07 +0200

    Add an failing example in main script

--- main.py
+++ main.py
@@ -10,9 +10,14 @@ def main():
     print(arithmetic_arranger(problems1))
 
     # Example 2
-    problems2 = ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]
+    problems2 = ['98 + 3g5', '3801 - 2', '45 + 43', '123 + 49']
     print("Problems 2:", problems2, "with answers.\n")
     print(arithmetic_arranger(problems2, True))
+
+    # Example 3
+    problems3 = ["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]
+    print("Problems 3:", problems3, "without answers.\n")
+    print(arithmetic_arranger(problems3))
 
     # A few more examples can be added here for demonstration purposes
 
